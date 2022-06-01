bff4f706fc23f8acb5d4ac78d2a18858d157239c
Ales Novotny <hi@alesnovotny.cz>
2022-06-01 16:12:54 +0200

    ahoj mami

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+
+app.UseRouting();
 
 app.Run();
 
